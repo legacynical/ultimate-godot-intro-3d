c7d9f8e601e518e1292ee0b9e5f8434763084d47
Legacynical <legacynical@gmail.com>
2025-01-20 09:14:05 -0800

    add jump test

--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -1,8 +1,15 @@
 extends CharacterBody3D
 
+@export var jump_height: float = 2.25
+@export var jump_time_to_peak: float = 0.4
+@export var jump_time_to_descent: float = 0.3
+
+@onready var jump_velocity: float = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
+@onready var jump_gravity: float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
+@onready var fall_gravity: float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
+
 @export var base_speed := 4.0
 @export var camera: Node3D
-@export var controller_index: int = 0
 
 var movement_input := Vector2.ZERO
 
@@ -12,17 +19,22 @@ func _ready() -> void:
 		print("No controllers detected.")
 	else:
 		print("Connected controllers: ", connected_devices)
-		print("Using controller index: ", controller_index)
 
 func _input(event):
 	if event is InputEventJoypadMotion:
-		print("Joypad Motion Detected:", event.axis, "Value:", event.axis_value)
+		print("Joypad ", event.device," Motion Detected: ", event.axis, " Value: ", event.axis_value)
 	elif event is InputEventJoypadButton:
-		print("Joypad Button Pressed:", event.button_index, "Pressed:", event.pressed)
+		print("Joypad ", event.device,"  Button Pressed: ", event.button_index, " Pressed: ", str(event.pressed))
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	movement_input = Input.get_vector("left", "right", "forward", "backward").rotated(-camera.global_rotation.y)
-	velocity = Vector3(movement_input.x, 0, movement_input.y) * base_speed
+	#velocity = Vector3(movement_input.x, 0, movement_input.y) * base_speed
+	
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = -jump_velocity
+	var gravity = jump_gravity if velocity.y > 0.0 else fall_gravity
+	velocity.y -= gravity * delta
+	
 	move_and_slide()
 	
 	#print(camera.global_rotation.y)
